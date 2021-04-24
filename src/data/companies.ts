d6e00624ddc9fe2d461f03bec2e04a2493c2e2b8
Nikos Katsikanis <nkatsikanis@gmail.com>
2021-04-24 11:32:26 +0200

    media sources

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -1,8 +1,13 @@
+import { getCountry2 } from '../data/countries';
+import type { country } from '../data/countries';
+
 export type Company = {
 	name: string;
 	industry: industry;
 	requiresPassport?: boolean;
-	offersBenefits?: boolean;
+	optionalBenefits?: boolean;
+	hqLocation: country;
+	optionalBenefitsSources?: [string, string];
 };
 
 export enum industry {
@@ -10,5 +15,15 @@ export enum industry {
 }
 
 export const companies: Company[] = [
-	{ name: 'BritishAirways', industry: industry.Airline, offersBenefits: true }
+	{
+		name: 'BritishAirways',
+		industry: industry.Airline,
+		optionalBenefits: true,
+		hqLocation: getCountry2('GB'),
+		optionalBenefitsSources: [
+			'https://www.cityam.com/british-airways-to-launch-own-vaccine-passport/',
+			'https://www.bbc.com/news/business-56392570'
+		]
+	}
 ];
+debugger;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,27 @@
 <script>
 	import { companies } from '../data/companies';
+	import { industry } from '../data/companies';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <table>
-	<thead><td>Name</td><</thead>
+	<thead
+		><tr><th>Name</th><th>HQ Location</th><th>Industry</th><th>Optional Benefits</th></tr></thead
+	>
 	{#each companies as item}
 		<tr>
 			<td>{item.name}</td>
-			<td>{item.}</td>
-			<td>{item.name}</td>
-			<td>{item.name}</td>
-			<td />
-			<td />
-			<td />
+			<td>{item.hqLocation.name}</td>
+			<td> {industry[item.industry]}</td>
+			<td
+				><span>{item.optionalBenefits}</span>
+				<a target="_blank" href={item.optionalBenefitsSources[0]}>Source 1</a><a
+					target="_blank"
+					href={item.optionalBenefitsSources[1]}>Source 2</a
+				></td
+			>
 		</tr>
 	{/each}
 </table>
